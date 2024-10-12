ebcc5aa926a1c60bfa824a611fab01c7cef822a4
Martin Muzatko <martin.joseph.ronald.muzatko@externe.dfs.de>
2024-10-12 23:37:27 +0200

    add design ontes

---
+++ src/components/readme.md.m
@@ -0,0 +1,45 @@
+Upgrade Tree with a central power/resource generator.
+Power is constant - e.g. it produces 10 - you can spend it
+
+
+Upgrades:
+
+Power generation
+
+Upgrade Areas:
+Pipes
+	Health
+	Corrossive Pipes (enemies take damage when attacking pipes)
+		Speed
+		Damage
+		Poison
+	Throughput (all or just this branch)
+	Tier
+Engine
+	Range
+	Health?
+	Power Generation
+	Tier
+Upgrade Nodes
+	Health
+	Shoot bullets
+		Speed
+		Size
+		Damage
+	Cost (reduce)
+Sites
+	Battery (allows to store energy)
+	Increase battery size
+	Allow more Throughput
+
+Lure
+	Attract enemies to stronger nodes
+	Evenly distribute
+
+Attackers
+	Increase attacks (reward)
+
+Harvester
+	Automatically collects enemy rewards
+
+Place more upgrades close to engine
