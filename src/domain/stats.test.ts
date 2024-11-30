139d66a276658124289d88e2da35723abed5b444
Martin Muzatko <martin.joseph.ronald.muzatko@externe.dfs.de>
2024-11-30 23:17:05 +0100

    create stats calc function that almost works and added tests

--- src/domain/stats.test.ts
+++ src/domain/stats.test.ts
@@ -1,42 +1,96 @@
 import { expect, test } from 'vitest'
-import { getActiveStats } from './stats'
+import { addStats, getActiveStats, mergeStats } from './stats'
 import { createUpgrade } from './upgrade'
 import { INITIAL_STATS } from '../data/initialGameData'
 
-test('stats filter where filter is true', () => {
-	const testUpgrade = createUpgrade({
-		id: '1',
-		active: true,
-		effect: (stats) => ({
-			upgradeArmor: {
-				value: stats.upgradeArmor + 1,
-				filter: () => true,
-			},
-		}),
-	})
-	expect(
-		getActiveStats([testUpgrade], [], { ...INITIAL_STATS, upgradeArmor: 0 })
-	).toStrictEqual({
-		...INITIAL_STATS,
-		upgradeArmor: 1,
+test('calculate stats for global', () => {
+	const x = getActiveStats(
+		[
+			createUpgrade({
+				active: true,
+				effect: (stats) => [
+					{
+						stats: {
+							upgradeArmor: stats.upgradeArmor + 1,
+						},
+					},
+				],
+			}),
+		],
+		[],
+		{ ...INITIAL_STATS, upgradeArmor: 2 }
+	)
+	expect(x).toStrictEqual({
+		globalStats: {
+			...INITIAL_STATS,
+			upgradeArmor: 3,
+		},
+		upgradeStats: new Map(),
 	})
 })
 
-test('stats filter where filter is false', () => {
-	const testUpgrade = createUpgrade({
-		id: '1',
-		active: true,
-		effect: (stats) => ({
-			upgradeArmor: {
-				value: stats.upgradeArmor + 1,
-				filter: () => false,
-			},
-		}),
+test('calculate stats for upgrade', () => {
+	const x = getActiveStats(
+		[
+			createUpgrade({
+				active: true,
+				id: 'A',
+				effect: (stats) => [
+					{
+						stats: {
+							upgradeArmor: stats.upgradeArmor + 3,
+						},
+					},
+				],
+			}),
+			createUpgrade({
+				active: true,
+				id: 'B',
+				effect: (stats) => [
+					{
+						filter: (upgrade) => upgrade.id === 'A',
+						stats: {
+							upgradeArmor: stats.upgradeArmor + 5,
+						},
+					},
+				],
+			}),
+		],
+		[],
+		{ ...INITIAL_STATS, upgradeArmor: 2 }
+	)
+	expect(x).toStrictEqual({
+		globalStats: {
+			...INITIAL_STATS,
+			upgradeArmor: 5,
+		},
+		upgradeStats: new Map([
+			[
+				'A',
+				{
+					upgradeArmor: 10,
+				},
+			],
+		]),
 	})
-	expect(
-		getActiveStats([testUpgrade], [], { ...INITIAL_STATS, upgradeArmor: 0 })
-	).toStrictEqual({
-		...INITIAL_STATS,
-		upgradeArmor: 0,
+})
+
+test('merge stats', () => {
+	const result = mergeStats({ mouseSize: 1 }, { mouseSize: 3 })
+	expect(result).toStrictEqual({ mouseSize: 3 })
+})
+
+test('add stats', () => {
+	const result = addStats(
+		{
+			maxPower: 1,
+		},
+		{
+			bulletMaxAmmo: 1,
+		}
+	)
+	expect(result).toStrictEqual({
+		maxPower: 1,
+		bulletMaxAmmo: 1,
 	})
 })
--- src/domain/stats.ts
+++ src/domain/stats.ts
@@ -1,6 +1,7 @@
 import { Connection } from './connection'
 import { createUpgrade, Upgrade } from './upgrade'
 
+/** will be executed for each upgrade */
 type TargetFilterFunction = (
 	upgrade: Upgrade,
 	upgrades: Upgrade[],
@@ -141,17 +142,21 @@ export const statDefinitions = {
 export type StatKeys = keyof typeof statDefinitions
 
 type GlobalStatKeys = {
-	[K in keyof typeof statDefinitions]: typeof statDefinitions[K]['type'] extends StatType.global ? K : never;
-}[keyof typeof statDefinitions];
+	[K in keyof typeof statDefinitions]: (typeof statDefinitions)[K]['type'] extends StatType.global
+		? K
+		: never
+}[keyof typeof statDefinitions]
 
 type UpgradeStatKeys = {
-	[K in keyof typeof statDefinitions]: typeof statDefinitions[K]['type'] extends StatType.scoped ? K : never;
-}[keyof typeof statDefinitions];
+	[K in keyof typeof statDefinitions]: (typeof statDefinitions)[K]['type'] extends StatType.scoped
+		? K
+		: never
+}[keyof typeof statDefinitions]
 
-// filter: TargetFilterFunction
+type UpgradeStatsEffectStats = Partial<Record<UpgradeStatKeys, number>>
 
 type UpgradeStatsEffect = {
-	stats: Partial<Record<UpgradeStatKeys, number>>
+	stats: UpgradeStatsEffectStats
 	filter: TargetFilterFunction
 }
 
@@ -162,6 +167,19 @@ type GlobalStatsEffect = {
 export type StatsEffect = UpgradeStatsEffect | GlobalStatsEffect
 
 export type Stats = Record<StatKeys, number>
+export type UpgradeStats = Record<UpgradeStatKeys, number>
+
+type StatsEffectResult = {
+	globalStats: Stats
+	upgradeStats: Map<string, UpgradeStats>
+}
+
+type IntermediateStatsEffectResult = {
+	globalStats: Partial<Stats>
+	upgradeStats: Map<string, Partial<UpgradeStats>>
+}
+
+type ResolvedStats = Map<string, Stats>
 
 /** subtracts a from b */
 export const diffStats = (a: Stats, b: Stats): Stats => ({
@@ -174,56 +192,105 @@ export const diffStats = (a: Stats, b: Stats): Stats => ({
 })
 
 /** adds b to a  */
-export const addStats = (a: Stats, b: Stats): Stats => ({
+export const addStats = (
+	a: Partial<Stats>,
+	b: Partial<Stats>
+): Partial<Stats> => ({
 	...(Object.fromEntries(
 		[...new Set([...Object.keys(a), ...Object.keys(b)])].map((key) => [
 			key,
-			a[key as keyof Stats] + b[key as keyof Stats],
+			(a[key as keyof Stats] ?? 0) + (b[key as keyof Stats] ?? 0),
 		])
 	) as Stats),
 })
 
-const NULL_FILTER_UPGRADE = () =>
-	createUpgrade({
-		id: 'NULL_FILTER_UPGRADE',
-		effect: () => [],
-	})
+export const mergeStats = (
+	a: Partial<Stats>,
+	b: Partial<Stats>
+): Partial<Stats> => ({
+	...(Object.fromEntries(
+		[...new Set([...Object.keys(a), ...Object.keys(b)])].map((key) => [
+			key,
+			b[key as keyof Stats] ?? a[key as keyof Stats],
+		])
+	) as Stats),
+})
 
+/**
+ * Get stats for all upgrades
+ */
 export const getActiveStats = (
 	upgrades: Upgrade[],
 	connections: Connection[],
-	initialStats: Stats,
-	filterUpgrade?: Upgrade
-): Stats => {
+	initialStats: Stats
+): StatsEffectResult => {
 	const activeUpgrades = upgrades.filter((node) => node.active)
-	return activeUpgrades.reduce((acc, upgrade) => {
-		const upgradeStats = upgrade.effect(acc, upgrade, upgrades)
-		return {
-			...initialStats,
-			...Object.fromEntries(
-				Object.entries(upgradeStats).map(([key, stat]) => [
-					key,
-					typeof stat == 'number'
-						? stat
-						: stat.filter(
-								filterUpgrade ?? NULL_FILTER_UPGRADE(),
+	return activeUpgrades.reduce(
+		(acc, upgrade) => {
+			const stats = addStats(
+				acc.globalStats,
+				acc.upgradeStats.get(upgrade.id) || {}
+			) as Stats
+			const statsEffects = upgrade.effect(stats, upgrade, upgrades)
+			const statsEffectResult = statsEffects.reduce(
+				(acc, statsEffect) => {
+					const shouldFilter = 'filter' in statsEffect
+					if (!shouldFilter)
+						return {
+							globalStats: mergeStats(
+								acc.globalStats,
+								statsEffect.stats
+							),
+							upgradeStats: acc.upgradeStats,
+						} satisfies IntermediateStatsEffectResult
+					const upgradeStats = new Map(
+						upgrades.flatMap((upgrade) => {
+							const doesEffect = statsEffect.filter(
+								upgrade,
 								upgrades,
 								connections
-						  )
-						? stat.value
-						: acc[key as keyof Stats],
-				])
-			),
-		} as Stats
-	}, initialStats)
+							)
+							return doesEffect
+								? [[upgrade.id, statsEffect.stats]]
+								: []
+							// ({upgradeId: upgrade.id, filter: filter(upgrade)})
+						}) as [string, UpgradeStatsEffectStats][]
+					)
+					return {
+						globalStats: acc.globalStats,
+						upgradeStats: new Map(
+							upgradeStats
+								.entries()
+								.map(([upgradeId, upgradeStat]) => [
+									upgradeId,
+									addStats(
+										acc.upgradeStats.get(upgradeId) ?? {},
+										upgradeStat
+									),
+								])
+						),
+					} satisfies IntermediateStatsEffectResult
+				},
+				{
+					globalStats: initialStats,
+					upgradeStats: new Map<string, UpgradeStats>(),
+				} as IntermediateStatsEffectResult
+			) as StatsEffectResult
+			return statsEffectResult
+		},
+		{
+			globalStats: initialStats,
+			upgradeStats: new Map(),
+		} as StatsEffectResult
+	)
 }
 
-export const getActiveGlobalStats = (
-	upgrades: Upgrade[],
-	connections: Connection[],
-	initialStats: Stats
-): Stats =>
-	getActiveStats(upgrades, connections, initialStats, NULL_FILTER_UPGRADE())
+// export const getActiveGlobalStats = (
+// 	upgrades: Upgrade[],
+// 	connections: Connection[],
+// 	initialStats: Stats
+// ): Stats =>
+// 	getActiveStats(upgrades, connections, initialStats, NULL_FILTER_UPGRADE())
 
 export const getCost = (stats: Stats, upgrade: Upgrade): number =>
 	Math.ceil(upgrade.cost * stats.upgradeCostMultiplier)
